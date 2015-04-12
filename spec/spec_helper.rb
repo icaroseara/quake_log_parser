c9891290f93999b8ce01678d8a9a78a495b266b6
Icaro Seara <icaro.seara@gmail.com>
2015-04-12 15:27:49 -0300

    setup codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 require 'quake_log_parser'
 require 'pry'
 require 'rspec/collection_matchers'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
