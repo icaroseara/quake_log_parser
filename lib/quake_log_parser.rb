ffe2b42aa7426b7a6dcbb30aff032a20a0719cd5
Icaro Seara <icaro.seara@gmail.com>
2015-04-12 01:49:20 -0300

    added means_of_death

--- lib/quake_log_parser.rb
+++ lib/quake_log_parser.rb
@@ -1,4 +1,5 @@
 require "quake_log_parser/version"
+require "quake_log_parser/means_of_death"
 require "quake_log_parser/parser"
 
 module QuakeLogParser
---
+++ spec/quake_log_parser/means_of_death_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe QuakeLogParser do
+  describe "means_of_death" do
+    it { expect(QuakeLogParser::MEANS_OF_DEATH).to have(29).means_of_death }
+  end
+end
